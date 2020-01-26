26769141c1229db836a89baa1bcf1fe4be91a0ad
Tadeo <tmenichel@gmail.com>
2020-01-26 12:15:08 -0500

    Update Dialogue
    Might not finish implementing the dialogue classes, since Cradle is basically already do that, albeit in a very unreadable or user friendly way.

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private string characterName;
+    public int CharacterName
+    {
+        get { return playerRelation; }
+    }
+    private int playerRelation;
+
+    public int PlayerRelation
+    {
+        get { return playerRelation; }
+        set { playerRelation = value; }
+    }
+
+    private Sprite portrait;
+    private Sprite fullBody;
+
+    public Character(string name)
+    {
+        characterName = name;
+        PlayerRelation = 0;
+
+        // get portrait and fullBody from character name
+    }
+}
---
+++ Assets/Scripts/Cradle Translate/UController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UController : MonoBehaviour
+{
+    public UStory currentStory;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeStory(UStory newStory)
+    {
+        currentStory = newStory;
+    }
+}
