32ee18dbb6f2453ec95cc02279603e3c49038a37
Tadeo <tmenichel@gmail.com>
2020-02-06 16:57:28 -0500

    Not Working

--- Assets/DialogueManager.cs
+++ Assets/DialogueManager.cs
@@ -16,26 +16,25 @@ public class DialogueManager : MonoBehaviour
     void Start()
     {
         DontDestroyOnLoad(this);
-        GetVariables();
-        var temp = flowchartVariables[goToNextIndex].GetValue();
-        Debug.Log(temp);
 
         flowchart = GameObject.FindGameObjectWithTag("dialogue").GetComponent<Flowchart>();
-        
+        GetVariables();
+
         // at the end of using a flowchart, check the values of the relationship meter variables and add them to the corresponding character
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        var temp = flowchartVariables[goToNextIndex].GetValue();
-        if(temp is System.Boolean)
+        if(flowchartVariables.Count > 0)
         {
-            if (check && (bool)temp)
-                GetNextDialogue();
-        }
-        
+            var temp = flowchartVariables[goToNextIndex].GetValue();
+            if (temp is System.Boolean)
+            {
+                if (check && (bool)temp)
+                    GetNextDialogue();
+            }
+        }        
     }
 
     void GetNextDialogue()
